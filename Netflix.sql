d3d395ddd68e7726cfaf4ce981e841245f66ac74
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-06-19 23:05:24 +0700

    Create Netflix.sql

---
+++ Netflix.sql
@@ -0,0 +1,22 @@
+with cte as
+(select *, 
+case when position('//' in Title) = 0 then Title else
+left (Title,(position('//' in Title)-1)) end as Eng_Title
+	from Netflix),
+-- Tách season
+cte2 as
+(select *, 
+case when position(': Season' in Title) = 0 then 'Movie' else
+'Series' end as film_Category
+	from cte),
+-- Tách title
+cte3 as
+(select *, 
+case when film_category = 'Movie' then eng_title 
+else left(eng_title, position(': Season' in Title)-1) end as only_title
+	from cte2)
+-- kiểm tra xem còn sót season trong tên không
+select title, eng_title, only_title, hours_viewed
+from cte3
+where only_title like '%Season%' or only_title like '%season%'
+order by hours_viewed desc
