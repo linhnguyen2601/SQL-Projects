6930cf51ee20b64277fc24e27191d8d3312f6122
Nguyen Thuy Linh <166676829+linhnguyen2601@users.noreply.github.com>
2024-07-04 09:15:51 +0700

    Update Customer Segmentation.sql

--- Customer Segmentation RFM/Customer Segmentation.sql
+++ Customer Segmentation RFM/Customer Segmentation.sql
@@ -24,7 +24,13 @@ create table SALES_DATASET_RFM_PRJ
   dealsize         VARCHAR
 ) 
 
--- 2. Đổi kiểu dữ liệu
+CREATE TABLE segment_score
+(
+    segment Varchar,
+    scores Varchar)
+
+	
+-- 2. Change data type
 ALTER TABLE SALES_DATASET_RFM_PRJ
 ALTER COLUMN ordernumber TYPE integer USING ordernumber::integer,
 ALTER COLUMN quantityordered TYPE integer USING quantityordered::integer,
@@ -35,6 +41,9 @@ ALTER COLUMN orderdate TYPE date USING orderdate::date,
 ALTER COLUMN msrp TYPE integer USING msrp::integer
 
 -- 3. Data cleaning
+-- 3.1 Check null/blank
+Table public.sales_dataset_rfm_prj
+	
 select * from public.sales_dataset_rfm_prj
 where ordernumber is null
 
@@ -53,7 +62,35 @@ where sales is null
 select * from public.sales_dataset_rfm_prj
 where orderdate is null
 
+-- 3.2. Check and remove duplicates
 
+
+-- 3.3. Determine outliers with Z-Score
+with cte as
+	(
+select *,
+(select avg(quantityordered) from sales_dataset_rfm_prj),
+(select stddev(quantityordered) from sales_dataset_rfm_prj)
+from sales_dataset_rfm_prj)
+
+select quantityordered, (quantityordered - avg)/stddev as z_score from cte
+where abs((quantityordered - avg)/stddev) > 3
+
+--4. CREATE NEW TABLE: SALES_DATASET_RFM_PRJ_CLEAN after cleaning data
+CREATE TABLE sales_dataset_rfm_clean as 
+	
+with cte as
+	(
+select *,
+(select avg(quantityordered) from sales_dataset_rfm_prj),
+(select stddev(quantityordered) from sales_dataset_rfm_prj)
+from sales_dataset_rfm_prj)
+
+select * from cte
+where abs((quantityordered - avg)/stddev) <= 3
+
+-- 5. RFM ANALYSIS
+-- 5.1. Calculate Recency, Frequency and Monetary Values
 select customername, 
 	current_date - max(orderdate) as recency,
 	count(distinct(ordernumber)) as frequency,
@@ -61,3 +98,26 @@ select customername,
 	from sales_dataset_rfm_clean
 where status = 'Shipped'
 group by customername
+
+-- 5.2. Divide RFM Values into 5 tiers using NTILE and form the rfm_scores using CONCAT:
+with cte as
+	(
+select customername, 
+	current_date - max(orderdate) as recency, 
+	count(distinct(ordernumber)) as frequency, 
+	sum(quantityordered * priceeach) as monetary
+	from sales_dataset_rfm_clean
+where status = 'Shipped'
+group by customername
+	),
+cte2 as 
+	(
+select customername, recency,
+ntile(5) over(order by recency desc) as R, frequency,
+ntile(5) over(order by frequency) as F, monetary,
+ntile(5) over(order by monetary) as M
+from cte
+	)
+select customername, concat(r,f,m) as rfm_scores from cte2 
+
+-- 5.3. JOIN table cte3 with table segment_score to categorize customers
