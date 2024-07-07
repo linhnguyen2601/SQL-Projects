9f85f683c9142e41f4f808ba2e45b85ee723277a
Nguyen Thuy Linh <166676829+linhnguyen2601@users.noreply.github.com>
2024-07-07 10:16:58 +0700

    Update Ecommerce TheLook.sql

--- Ecommerce: (EDA) and Cohort Analysis/Ecommerce TheLook.sql
+++ Ecommerce: (EDA) and Cohort Analysis/Ecommerce TheLook.sql
@@ -137,9 +137,47 @@ group by month_year
 order by month_year desc
 
  **3.2. Average Order Value (AOV) and the number of customers per month** 
+with cte as (
 select a.order_id, a.created_at, b.user_id, b.product_id, b.sale_price
 from bigquery-public-data.thelook_ecommerce.orders as a
 join bigquery-public-data.thelook_ecommerce.order_items as b
 on a.order_id = b.order_id
 where a.status = 'Complete' and a.created_at < '2024-07-01'
-order by a.order_id
+order by a.order_id)
+
+select format_date('%Y-%m', created_at) as month_year,
+sum(sale_price)/count(distinct(order_id)) as AOV,
+count(distinct(user_id)) as user_number from cte
+group by month_year
+order by month_year desc
+
+**3.3. Customer segmentation by age and gender**
+
+ select age, 
+sum(case when gender = 'M' then 1 else 0 end) as Male,
+sum(case when gender = 'F' then 1 else 0 end) as Female,
+count(id)  from bigquery-public-data.thelook_ecommerce.users
+where id in (Select user_id from bigquery-public-data.thelook_ecommerce.orders where status = 'Complete' and created_at < '2024-07-01') 
+group by age
+order by age
+
+ **3.4. Customer segmentation by geography/country**
+
+select country, 
+sum(case when gender = 'M' then 1 else 0 end) as Male,
+sum(case when gender = 'F' then 1 else 0 end) as Female,
+count(id) as total from bigquery-public-data.thelook_ecommerce.users
+where id in (Select user_id from bigquery-public-data.thelook_ecommerce.orders where status = 'Complete' and created_at < '2024-07-01') 
+group by country
+order by total desc
+
+ **3.5. Customer segmentation by traffic source**
+
+select traffic_source, 
+sum(case when gender = 'M' then 1 else 0 end) as Male,
+sum(case when gender = 'F' then 1 else 0 end) as Female,
+count(id) as total from bigquery-public-data.thelook_ecommerce.users
+where id in (Select user_id from bigquery-public-data.thelook_ecommerce.orders where status = 'Complete' and created_at < '2024-07-01') 
+group by traffic_source
+order by total desc
+
