4dda3d0de269450e8a65ec62d81ddab785b3264f
Nguyen Thuy Linh <166676829+linhnguyen2601@users.noreply.github.com>
2024-07-07 12:07:05 +0700

    Create Online Retail: Cohort Analysis.sql

---
+++ Online Retail/Online Retail: Cohort Analysis.sql
@@ -0,0 +1,13 @@
+1. Tạo bảng và import data
+CREATE TABLE online_retail
+(
+    invoiceno varchar,
+    stockcode varchar,
+    description varchar,
+    quantity varchar,
+    invoicedate varchar,
+    unitprice varchar,
+    customerid varchar,
+    country varchar
+)
+
