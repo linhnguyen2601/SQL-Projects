af59ca84ebed6c6d1c2839db82c4ae24e2f80e50
Nguyen Thuy Linh <166676829+linhnguyen2601@users.noreply.github.com>
2024-07-07 17:02:36 +0700

    Update Online Retail: Cohort Analysis.sql

--- Online Retail: Cohort Analysis/Online Retail: Cohort Analysis.sql
+++ Online Retail: Cohort Analysis/Online Retail: Cohort Analysis.sql
@@ -1,4 +1,4 @@
-1. Tạo bảng và import data
+1. CREATE table and import data
 CREATE TABLE online_retail
 (
     invoiceno varchar,
@@ -10,4 +10,26 @@ CREATE TABLE online_retail
     customerid varchar,
     country varchar
 )
+
+2. CHANGE data type
+
+ALTER TABLE online_retail
+ALTER COLUMN quantity type integer USING quantity::integer,
+ALTER COLUMN invoicedate type date USING invoicedate::date,
+ALTER COLUMN unitprice type float USING unitprice::double precision
+
+3. Check null
+
+select * from online_retail
+where invoiceno is null 
+or stockcode is null
+or description is null
+or quantity is null
+or invoicedate is null
+or unitprice is null
+or customerid is null
+or country is null
+
+=> There is no null value
+
 
